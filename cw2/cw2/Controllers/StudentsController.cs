ee50466bb654d6ef6712b54486018ccae144b1d3
unknown <s16502@pjwstk.edu.pl>
2020-03-22 23:04:11 +0100

    przygotowanie projektu

---
+++ cw2/cw2/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw2.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudent()
+        {
+            return "kowalski, nowak";
+        }
+    }
+}
