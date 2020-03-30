9ea525bac29bd3ef1f2bd1b49e6d69c36ee8922b
unknown <s16502@pjwstk.edu.pl>
2020-03-30 22:20:37 +0200

    cw3 1-4.2

--- cw2/cw2/Controllers/StudentsController.cs
+++ cw2/cw2/Controllers/StudentsController.cs
@@ -2,40 +2,83 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using cw2.Models;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using cw2.Models;
+using System.Data.SqlClient;
 
-namespace cw2.Controllers
+
+
+namespace cwiczenia2.Controllers
 {
     [Route("api/students")]
     [ApiController]
     public class StudentsController : ControllerBase
     {
+        string conString = "Data Source=db-mssql;Initial Catalog=s16502; Integrated Security=True";
+
+        public StudentsController()
+        {
+        }
+
         [HttpGet]
-        public string GetStudent(string orderBy)
+        public IActionResult GetStudents(string orderBy)
         {
-            return $"kowalski, nowak sortowanie={orderBy}";
+            var list = new List<StudentInfoDTO>();
+            using (SqlConnection con = new SqlConnection(conString))
+            using (SqlCommand com = new SqlCommand())
+            {
+                com.Connection = con;
+                com.CommandText = "select s.FirstName, s.LastName, s.BirthDate, st.Name, e.Semester from Student s join Enrollment e on e.IdEnrollment = s.IdEnrollment join Studies st on st.IdStudy = e.IdStudy";
+                con.Open();
+
+                SqlDataReader dr = com.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    var st = new StudentInfoDTO
+                    {
+                        FirstName = dr["FirstName"].ToString(),
+                        LastName = dr["LastName"].ToString(),
+                        BirthDate = dr["BirthDate"].ToString(),
+                        Name = dr["Name"].ToString(),
+                        Semester = dr["Semester"].ToString()
+                    };
+                    list.Add(st);
+                }
+
+            }
+            return Ok(list);
         }
+
         [HttpGet("{id}")]
-        public IActionResult GetStudent(int id)
+        public IActionResult GetStudent([FromRoute]string indeks)
         {
-            if (id == 1)
+            var st = new StudentInfoDTO();
+            using (SqlConnection con = new SqlConnection(conString))
+            using (SqlCommand com = new SqlCommand())
             {
-                return Ok("Kowalski");
+                com.Connection = con;
+                com.CommandText = "select s.FirstName, s.LastName, s.BirthDate, st.Name, e.Semester from Student s join Enrollment e on e.IdEnrollment = s.IdEnrollment join Studies st on st.IdStudy = e.IdStudy";
+                con.Open();
+
+                SqlDataReader dr = com.ExecuteReader();
+
+                st.FirstName = dr["FirstName"].ToString();
+                st.LastName = dr["LastName"].ToString();
+                st.BirthDate = dr["BirthDate"].ToString();
+                st.Name = dr["Name"].ToString();
+                st.Semester = dr["Semester"].ToString();
             }
-            else if (id == 2)
-            {
-                return Ok("Nowak");
-            }
-            
-            return NotFound("Nie znalezniono studenta");
+            return Ok(st);
         }
+
         [HttpPost]
         public IActionResult CreateStudent(Student student)
         {
             student.IndexNumber = $"s{new Random().Next(1, 20000)}";
             return Ok(student);
         }
+
     }
 }
